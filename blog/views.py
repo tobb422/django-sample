6af8ccd980e4857d6feef176664e6bb78b348abf
Toshiki Baba <yz.t0.r33@gmail.com>
2019-01-11 08:28:48 +0900

    bloglistview

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Blog
 
-# Create your views here.
+class BlogListView(ListView):
+    model = Blog
