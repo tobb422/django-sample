1cac320b5c9356ab6f43c8d9ae391be86e767f2b
Toshiki Baba <yz.t0.r33@gmail.com>
2019-01-11 07:57:43 +0900

    モデル作成

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    content = models.CharField(max_length=140, verbose_name='本文')
+    posted_date = models.DateTimeField(auto_now_add=True)
